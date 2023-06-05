0afb8f1314628341ea6fc858d7f68de4a6396fc4
Allen <allenthomas204@gmail.com>
2023-06-05 23:53:12 +0530

    Update farmerController.js
    add login functionality

--- backend/controller/farmerController.js
+++ backend/controller/farmerController.js
@@ -6,12 +6,39 @@ exports.register = async (req, res, next) => {
         const { name,phone,password,district,taluk,block,kb,wardno } = req.body;
         //const duplicate = await farmerServices.getUserByPhone(phone);
        /* if (duplicate) {
-            throw new Error(`UserName ${email}, Already Registered`)
+            throw new Error(`UserName ${phone}, Already Registered`)
         }*/
         const response = await farmerService.registerFarmer(name,phone,password,district,taluk,block,kb,wardno);
         res.json({ status: true, success: 'Farmer registered successfully' });
     } catch (err) {
         console.log("---> err -->", err);
         next(err);
+    }
+}
+
+//login checking.....................
+exports.login = async (req, res, next) => {
+    try {
+        const { phone, password } = req.body;
+        if (!phone || !password) {
+            throw new Error('credentials are not correct');
+        }
+        let user = await farmerService.checkUser(phone);
+        if (!user) {
+            throw new Error('User does not exist');
+        }
+        const isPasswordCorrect = await user.comparePassword(password);
+        if (isPasswordCorrect === false) {
+            throw new Error(`Username or Password does not match`);
+        }
+        // Creating Token
+        let tokenData;
+        tokenData = { _id: user._id, phone: user.phone };
+    
+        const token = await farmerService.generateAccessToken(tokenData,"secret","1h")
+        res.status(200).json({ status: true, success: "sendData", token: token });
+    } catch (error) {
+        console.log(error, 'err---->');
+        next(error);
     }
 }
