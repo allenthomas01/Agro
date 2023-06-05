b2a1fa306ebe922d3a9309ac0a60130217eb21b7
Allen <allenthomas204@gmail.com>
2023-06-05 23:52:54 +0530

    Update farmerModel.js
    add login functionality

--- backend/model/farmerModel.js
+++ backend/model/farmerModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const db = require('../config/db');
+const bcrypt = require('bcrypt');
 
 const { Schema }=mongoose;
 
@@ -39,6 +40,35 @@ const farmerSchema = new Schema({
         required: [true, "ward number is required"]
     }
 },{timestamps:true});
+
+
+farmerSchema.pre("save",async function(){
+    var user = this;
+    if(!user.isModified("password")){
+        return
+    }
+    try{
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(user.password,salt);
+
+        user.password = hash;
+    }catch(err){
+        throw err;
+    }
+});
+
+
+//used while signIn decrypt
+farmerSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        console.log('----------------no password',this.password);
+        // @ts-ignore
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (error) {
+        throw error;
+    }
+};
 
 const farmerModel = db.model('farmer',farmerSchema);
 
