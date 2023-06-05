8c4777de1658354d3c15689cb8e73d97e391f6d5
Allen <allenthomas204@gmail.com>
2023-06-05 15:24:11 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const body_parser = require('body-parser');
+const farmerRouter = require('./routers/farmerRouter');
 const app = express();
-
+app.use(body_parser.json());
+app.use('/',farmerRouter);
 module.exports=app;
