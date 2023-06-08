763b593e95582e3257ee7c65191158cce287060d
Allen <allenthomas204@gmail.com>
2023-06-09 00:30:19 +0530

    resolve error in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-const connection = mongoose.createConnection(`${process.env.DB_URL}`).on('open',()=>{
-    console.log("MongoDB atlas Connected");
 const connection = mongoose.createConnection(`mongodb+srv://team11:AgroConnect123@storage.jhabdnb.mongodb.net/?retryWrites=true&w=majority`).on('open',()=>{
     console.log("MongoDB Atlas Connected");
 }).on('error',()=>{
