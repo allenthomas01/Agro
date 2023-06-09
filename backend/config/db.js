2a653d9f86444246c5b4c2ac640dbcd841bede47
Allen <allenthomas204@gmail.com>
2023-06-09 11:35:11 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-const connection = mongoose.createConnection(`mongodb+srv://team11:AgroConnect123@storage.jhabdnb.mongodb.net/?retryWrites=true&w=majority`).on('open',()=>{
+const connection = mongoose.createConnection(`${process.env.DB_URL}`).on('open',()=>{
     console.log("MongoDB Atlas Connected");
 }).on('error',()=>{
     console.log("MongoDB Atlas Connection error");
